e1929424afe49537c4eefc7125b15929ac374520
mpisters <pistersmichelle@gmail.com>
2017-02-23 13:59:30 +0100

    improved algorithm

--- src/all/AddingWords.java
+++ src/all/AddingWords.java
@@ -7,55 +7,65 @@ public class AddingWords {
         Map<String, Integer> map = new HashMap<String, Integer>();
         Map<Integer, String> map2 = new HashMap<Integer, String>();
         while (sc.hasNextLine()) {
-            String cards = sc.nextLine();
-            String[] parts = cards.split(" ");
-            if (parts[0].equals("def")) {
-                map.put(parts[1], Integer.parseInt(parts[2]));
-                map2.put(Integer.parseInt(parts[2]), parts[1]);
+            LinkedList<String> list = new LinkedList<>();
+            String line = sc.nextLine();
+            String[] parts = line.split(" ");
+            for (int i =0; i < parts.length; i++){
+                list.add(parts[i]);
             }
-            if (parts[0].equals("calc")){
-                int prev = 0;
-                int curr = 0;
-                boolean isString = false;
-                for (int i = 1; i < parts.length; i++){
-                    String item = parts[i];
-                    if (!isString && !item.equals("+") && !item.equals("-") && !item.equals("=")){
-                        prev = map.containsKey(item) ? map.get(item) : -10000;
-                    }
-                    if (isString){
-                        isString = false;
-                        String nextItem = parts[i];
-                        curr = map.containsKey(nextItem) ? map.get(nextItem) : -10000;
-                        if (parts[i -1].equals("+")) {
-                            prev = curr != -10000 && prev != -10000 ? prev + curr : -10000;
+            Iterator<String> i = list.iterator();
+            String item = i.next();
+            int total = 0;
+            String unknown = "";
+            String symbol = "";
+            int count = 0;
+            while(i.hasNext()){
+                switch (item) {
+                    case "def":
+                        String key = i.next();
+                        int value = Integer.parseInt(i.next());
+                        map.put(key, value);
+                        map2.put(value, key);
+                        break;
+                    case "clear":
+                        map.clear();
+                        map2.clear();
+                        i.next();
+                        break;
+                    case "calc":
+                        String word = i.next();
+                        if (word.equals("+") || word.equals("-")){
+                            symbol = word;
                         }
-                        else{
-                            prev = curr != -10000 && prev != -10000 ? prev - curr : -10000;
-                        }
+                        if (word.length() > 1 && map.containsKey(word) && !unknown.equals("unknown")) {
+                            if (symbol.equals("+") || count == 0) {
+                                total += map.get(word);
 
-                    }
-                    if (item.equals("+")){
-                        isString = true;
-                    }
-                    if (item.equals("-")){
-                        isString = true;
-                    }
-                    if (item.equals("=")){
-                        String answer = map2.containsKey(prev) ? map2.get(prev) : "unknown";
-                        String result = "";
-                        for (int j = 1; j < parts.length; j++) {
-                            String word = parts[j];
-                            result = result + word + " ";
-
+                            } else {
+                                total -= map.get(word);
+                            }
+                        } else if (!map.containsKey(word) && !unknown.equals("unknown") && word.length() > 1){
+                            unknown += "unknown";
                         }
-                        System.out.println(result + answer);
-                    }
+                        count++;
+                        if (word.equals("=")){
+                            String answer = "";
+                            if (!unknown.equals("unknown") && map2.containsKey(total)){
+                                answer += map2.get(total);
+                            } else {
+                                answer += "unknown";
+                            }
+                            list.removeFirst();
+                            System.out.println(String.join(" ", list) + " " + answer);
+                            total = 0;
+                            unknown = "";
+                        }
+                        break;
+                    default:
+                        i.next();
                 }
-            }
-            if (parts[0].equals("clear")){
-                map.clear();
-                map2.clear();
             }
         }
     }
+
 }
